f736cedb486360d852e67f66fc27dcd3adba9bb8
김태근 <oryukdo3@pusan.ac.kr>
2024-05-03 17:15:51 +0900

    feat: 게시글 fouc 문제 개선

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,9 +4,47 @@
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-ssr/
  */
 
+const React = require("react");
+
 /**
  * @type {import('gatsby').GatsbySSR['onRenderBody']}
  */
-exports.onRenderBody = ({ setHtmlAttributes }) => {
-  setHtmlAttributes({ lang: `en` })
-}
+exports.onRenderBody = ({ setHeadComponents, setHtmlAttributes }) => {
+  setHtmlAttributes({ lang: `en` });
+
+  const criticalCss = `
+    .blog-post-wrapper {
+        width: 100%;
+    }
+    .blog-head-wrapper {
+        position: relative;
+        margin: 0 0 2rem 0;
+    }
+    .blog-head-line {
+        position: absolute;
+        z-index: 1;
+        top: 40%;
+        left: 50%;        
+        transform: translateX(-50%);
+    }
+    .blog-post header h1 {
+        width: 600px;
+        color: var(--color-white);
+        font-size: var(--fontSize-8);
+        margin: var(--spacing-0) var(--spacing-0) var(--spacing-4) var(--spacing-0);
+    }
+    .blog-post-article-image{
+        border-radius: var(--radius-0);
+        margin-bottom: 0rem;
+        width: 100%;
+        height: 60vh;
+        filter: brightness(50%);
+        object-fit: cover; 
+        object-position: center;
+    }
+  `;
+
+  setHeadComponents([
+    <style key="critical-css" dangerouslySetInnerHTML={{ __html: criticalCss }} />
+  ]);
+};
