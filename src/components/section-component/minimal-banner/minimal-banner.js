7fe076c5fc83dafaac48ba6541f65c84fab6e403
김태근 <oryukdo3@pusan.ac.kr>
2024-05-10 18:18:28 +0900

    fix: 로켓위치 반응형으로 조절

--- src/components/section-component/minimal-banner/minimal-banner.js
+++ src/components/section-component/minimal-banner/minimal-banner.js
@@ -1,18 +1,19 @@
-import React from "react";
-import "./minimal-banner.css"
-import Rocket from '../../../images/rocket.svg'
+import React, { useRef, useLayoutEffect, useState } from "react";
+import "./minimal-banner.css";
+import Rocket from '../../../images/rocket.svg';
 
 const MinimalBanner = () => {
+
   return (
     <div className="banner-container">
       <h1>
-        <img src={Rocket} alt="Rocket Flying Animation"></img>
         <p className="banner-title">
           <span>안녕하세요,</span>
           <span className="gradient-title"> 백엔드 개발자 </span>
           <span>김태근입니다.</span>
         </p>
       </h1>
+        <img src={Rocket} alt="Rocket Flying Animation"/>
     </div>
   );
 };
