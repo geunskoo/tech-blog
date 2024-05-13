cdad1fe5260aeba150f09f7304162d58efe0bc25
김태근 <oryukdo3@pusan.ac.kr>
2024-05-13 15:25:35 +0900

    feat: 모바일화면에서는 네비게이션 버튼 안보이도록 조치

--- src/components/core-component/layout/layout.js
+++ src/components/core-component/layout/layout.js
@@ -87,15 +87,16 @@ const Layout = ({ location, children }) => {
         </TransitionGroup>
         
         <div className={`lock ${showScrollButton ? "move-up" : "move-down"}`} >
-          <button style={{display:"flex", justifyContent:"center", alignItems:"center"}} onClick={handleOpenModal}><StaticImage style={{width:"22.5px", height:"22.5px", position:"relative"}} src= "../../../images/chat.png" alt="newtag" layout="fixed"/></button>
+          <button className='chat-button' onClick={handleOpenModal}><StaticImage style={{width:"22.5px", height:"22.5px", position:"relative"}} src= "../../../images/chat.png" alt="newtag" layout="fixed"/></button>
         </div>
         <a className={`github ${showScrollButton ? "move-up" : "move-down"}`} href="https://github.com/geunskoo" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
           <FaGithub size={25} />
         </a>
           <div className={`scroll-to-top ${showScrollButton ? "show" : "hide"}`} >
-            <button style={{display:"flex", justifyContent:"center", alignItems:"center"}} onClick={scrollToTop}><StaticImage style={{width:"25px", height:"25px", position:"relative"}} src= "../../../images/arrow.png" alt="newtag" layout="fixed"/></button>
+            <button className='chat-button' onClick={scrollToTop}><StaticImage style={{width:"25px", height:"25px", position:"relative"}} src= "../../../images/arrow.png" alt="newtag" layout="fixed"/></button>
           </div>
         </div>
+
         <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
           <h2 style={{margin:"1rem 0 1rem 0"}}>💬 소통해요! </h2>
           <p style={{margin: "0 0 2rem 1rem"}}>편안하게 이야기하고 소통해보아요~</p>
